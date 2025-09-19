9491898db0e6b07ed1f2abb655523a3250710739
hussainali <hussainalee@icloud.com>
2025-09-19 03:52:32 -0700

    nodemon replaced by tsc-watch package.

--- src/routers/default.routes.ts
+++ src/routers/default.routes.ts
@@ -1,9 +1,9 @@
 import { Request, Response, Router } from "express";
 import { health } from "../handlers/default.handler";
-import { verifyAccessToken } from "../middlewares/auth.middleware";
+// import { verifyAccessToken } from "../middlewares/auth.middleware";
 
 const routes = Router();
 
-routes.route("/healthz").get(verifyAccessToken, health);
+routes.route("/healthz").get(health);
 
 export default routes;
